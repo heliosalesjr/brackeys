3152311384f8a2e88ed279ce743322f4f74c35f2
heliosalesjr <heliosalesjr@gmail.com>
2024-05-01 11:43:49 -0300

    version 1.0 wrap up

--- scripts/coins.gd
+++ scripts/coins.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+@onready var pick_up_sound = $PickUpSound
 
 func _on_body_entered(body):
+	pick_up_sound.play()
+	game_manager.add_point()
+	
+func _on_pick_up_sound_finished():
 	queue_free()
